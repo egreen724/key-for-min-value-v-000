1d0be1e7975c911c28dd29b01ed08e4472f92079
Erin Green <erin.y.green@gmail.com>
2018-09-23 22:29:01 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,11 +9,6 @@ def key_for_min_value(name_hash)
      lowest_value = value 
      lowest_key = key
  end
-  lowest_key
  end 
-   
- if name_hash = {} 
-   return nil 
- end 
- 
+   lowest_key
 end
